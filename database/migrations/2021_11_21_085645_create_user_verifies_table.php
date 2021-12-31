c2482178c99db626e262a53825111d99554bafe1
Rafiul <rafiulkarim51@gmail.com>
2021-12-31 19:21:39 +0600

    user_verify table modify

--- database/migrations/2021_11_21_085645_create_user_verifies_table.php
+++ database/migrations/2021_11_21_085645_create_user_verifies_table.php
@@ -14,6 +14,7 @@ class CreateUserVerifiesTable extends Migration
     public function up()
     {
         Schema::create('user_verifies', function (Blueprint $table) {
+            $table->id();
             $table->integer('user_id');
             $table->string('token');
             $table->timestamps();
